5853f3e49140132815a02936c94678842fafeaa1
suren <zxjlwt@126.com>
2018-12-29 17:23:08 +0800

    add help file for http step

--- src/main/java/com/surenpi/jenkins/phoenix/steps/HttpStep.java
+++ src/main/java/com/surenpi/jenkins/phoenix/steps/HttpStep.java
@@ -1,9 +1,20 @@
 package com.surenpi.jenkins.phoenix.steps;
 
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
+import com.cloudbees.plugins.credentials.domains.DomainRequirement;
 import com.surenpi.jenkins.phoenix.DurableExecution;
 import com.surenpi.jenkins.phoenix.DurableStep;
 import com.surenpi.jenkins.phoenix.DurableTaskStepDescriptor;
 import hudson.Extension;
+import hudson.model.FreeStyleProject;
+import hudson.security.ACL;
+import hudson.util.ListBoxModel;
+import jenkins.model.Jenkins;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.jenkinsci.plugins.durabletask.DurableTask;
 import org.jenkinsci.plugins.workflow.steps.StepContext;
 import org.jenkinsci.plugins.workflow.steps.StepExecution;
@@ -11,6 +22,8 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 /**
@@ -55,6 +68,13 @@ public class HttpStep extends DurableStep implements Serializable
         public String getDisplayName()
         {
             return "HttpStep";
+        }
+
+        public ListBoxModel doFillMethodItems() {
+            ListBoxModel listBoxModel = new ListBoxModel();
+            listBoxModel.add(HttpPost.METHOD_NAME);
+            listBoxModel.add(HttpGet.METHOD_NAME);
+            return listBoxModel;
         }
     }
 
